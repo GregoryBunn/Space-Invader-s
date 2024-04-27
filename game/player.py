f1001e849b0b6152e1742cd19b9db1c987b63f73
Wikus236 <wikusvanbiljon236@gmail.com>
2024-04-27 19:48:26 +0200

    Comments

--- game/player.py
+++ game/player.py
@@ -11,6 +11,36 @@ import time
 
 
 class Player:
+    """
+    Represents a player character in the game with attributes for movement, aiming, and scoring.
+
+    Attributes:
+        _x (float): The x-coordinate of the player.
+        _y (float): The y-coordinate of the player.
+        _aimChange (float): Amount of change in aiming direction per update.
+        _aimDir (float): Current aiming direction in radians.
+        _moveDir (float): Current moving direction; -1 for left, 1 for right.
+        _moveSpeed (float): Speed of player movement.
+        _size (int): Size of the player, affecting collision detection.
+        _score (int): Current score of the player.
+        _time (int): Timer since the last missile was fired.
+        _missileTime (int): Required delay time between firing missiles.
+        _lives (int): Number of lives the player has.
+        _picList (list): List of images for different player states.
+        _picture (Picture): Current picture representing the player.
+
+    Methods:
+        RotatePlayer(aimdir): Rotates the player to a new aim direction based on aimdir.
+        drawPlayer(): Draws the player on the game screen.
+        movePlayer(): Updates the player's position based on their move direction and speed.
+        ChangeAim(): Changes the player's aiming direction within allowed bounds.
+        drawScore(PlayerNum): Displays the player's score on the screen.
+        drawLives(PlayerNum): Displays the number of lives left for the player.
+        draw_missileTimer(playerNum): Draws a timer that indicates the readiness of the next missile.
+        updateTime(): Increments the internal timer to manage missile firing readiness.
+        isAllowed(): Checks if the player is allowed to fire a missile based on the timer.
+    """
+    
     def __init__(self,x : float,y : float,aimchange : float,aimDirection : float,moveDirection : float,moveSpeed : float,size : int,score : int,time : int,missileTime:int,lives:int,pictures):
         
         self._x = x #player x coordinate
@@ -27,6 +57,8 @@ class Player:
         self._picList = pictures
         self._picture = self._picList[24]
 
+
+    #Function that rotates player by choosing an image based on aim direction
     def RotatePlayer(self,aimdir):
         pos = int((aimdir/(math.pi/2))*24) + 24
         self._picture = self._picList[pos]
@@ -92,7 +124,7 @@ class Player:
         direc = self._aimChange
         playerD = playerD +rate* direc
 
-        #test is aim is within bounds
+        #test if aim is within bounds
         if playerD < -(math.pi)/2:
             playerD = -(math.pi)/2
             self._aimChange = 0
@@ -103,6 +135,7 @@ class Player:
         #set new aim
         self._aimDir = playerD
     
+    #Draw score of player
     def drawScore(self,PlayerNum):
         stddraw.setFontSize(20)
         stddraw.setPenColor(stddraw.WHITE)
@@ -111,6 +144,7 @@ class Player:
         else:
             stddraw.text(80,-85,"score: "+str(self._score))
 
+    #Draw lives of player
     def drawLives(self,PlayerNum):
         
         stddraw.setPenColor(stddraw.WHITE)
@@ -123,6 +157,7 @@ class Player:
 
 
     def draw_missileTimer(self,playerNum):
+        #Draw the time since the last missile was fired for a player
         stddraw.setPenColor(stddraw.DARK_GRAY)
         
         if playerNum == 0:
@@ -130,20 +165,23 @@ class Player:
         else:
             stddraw.rectangle(94 ,-80,5,25) 
         
+        #Timer red if missile not allowed
         if self._time <= self._missileTime:
             stddraw.setPenColor(stddraw.RED)
+        #Timer green if missile is allowed
         elif self._time > self._missileTime and self._time < self._missileTime*2:
             stddraw.setPenColor(stddraw.GREEN)
         else:
+            #Timer Blue if super missile is allowed
             stddraw.setPenColor(stddraw.BLUE)
 
+        #Cap the height to double the allowed time
         if self._time > self._missileTime*2:
             h = self._missileTime*2
         else:
             h = self._time
 
         #scale h to max 25
-
         h = h/(self._missileTime*2) * 25
 
         if playerNum == 0:
@@ -167,6 +205,18 @@ class Player:
 
             
 class PlayerList:
+    """
+    Manages a collection of players in the game. Handles their drawing, movement, and interactions with game elements like missiles and powerups.
+
+    Methods:
+        add_player(player: Player): Adds a player to the list.
+        remove_player(player: Player): Removes a player from the list.
+        draw_ScorePlayers(): Draws all players along with their scores, missile timers, and lives.
+        move_aim_timeUpdate(): Updates the movement, aim, and timers for all players.
+        hitmarks_Players(Missiles_list): Checks and handles collisions between players and missiles.
+        hitmarks_Powerups(powerupList): Checks and handles collisions between players and powerups, activating any powerups.
+        activate_Powerup(typ, player): Activates a powerup effect based on the powerup type.
+    """
     def __init__(self):
         self._Players = []
     def add_player(self,player:Player):
@@ -182,6 +232,7 @@ class PlayerList:
             player.drawLives(c)
             c+=1#increase player num
 
+    #Itterate throung player and update their positions aim and time
     def move_aim_timeUpdate(self):
         for player in self._Players:
             player.movePlayer()
@@ -262,18 +313,21 @@ class PlayerList:
                 pc = pc + 1 # go to next powerup 
 
     def activate_Powerup(self,typ,player):
-        if typ == 1:
-            if player._missileTime < 45:
-            #if player.missileTime < 0:
-            
+
+        if typ == 1:#Activate type 1 missile
+            #Only allow if allow if time less than 45
+            if player._missileTime < 45: 
                 player._missileTime = 45
             else:
                 player._missileTime -= 10
-        if typ == 2:
+
+        if typ == 2:#type 2 missile
+            # only allow if live are less then 3
             if player._lives < 3:
                 player._lives += 1
 
-        if typ == 3:
+        if typ == 3:#Type 3 missile
+            #only allow if move speed is not to high
             if player._moveSpeed < 3.5:
                 player._moveSpeed += 0.5
             
